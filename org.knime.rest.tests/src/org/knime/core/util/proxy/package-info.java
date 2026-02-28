a944bf1b2bf9d0c31b30bb45e7d843853875d7fb
Leon Wenzler <leon.wenzler@knime.com>
2026-02-28 12:01:52 +0100

    AP-23138: Fix comment in package-info.java
    AP-23138 (Replace static internal proxy with sidecar in knime-rest testflows)

--- org.knime.rest.tests/src/org/knime/core/util/proxy/package-info.java
+++ org.knime.rest.tests/src/org/knime/core/util/proxy/package-info.java
@@ -1,5 +1,7 @@
 /**
+ * This package holds "integration" tests for the Apache HC4 and HC5 clients,
+ * using proxies supplied by the Eclipse proxy support in {@code org.eclipse.core.net}.
  *
- * @author lw
+ * @author Leon Wenzler, KNIME AG, Konstanz, Germany
  */
 package org.knime.core.util.proxy;
